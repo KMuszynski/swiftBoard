1a6b575bcb808740ded3eb526fe7ef471706569c
bnowacki <bknowacki@gmail.com>
2023-05-21 18:29:30 +0200

    company tables

--- migrations/01_companies.sql
+++ migrations/01_companies.sql
@@ -1,7 +1,7 @@
 -- migrate:up
 
 create table "companies" (
-  "id" uuid references auth."users" not null primary key,
+  "id" uuid default uuid_generate_v4() not null primary key,
   "name" text not null,
   "description" text not null,
   "documents" []jsonb
@@ -14,8 +14,17 @@ create table "company_users" (
   "company" uuid references "companies" not null
   primary key ("user", "company"),
 
-  "role" company_user_role not null default 'employee'
+  "role" company_user_role not null default 'employee',
 );
+
+create table "company_positions" (
+  "id" uuid references auth."users" not null primary key,
+  "company" uuid references "companies" not null,
+  "level" text not null, -- change to enum editable by employers
+  "responsibilities" []text
+);
+
+
 
 
 
