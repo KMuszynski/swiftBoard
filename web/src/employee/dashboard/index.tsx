459c0d68d819fcb9ca980b53682c5f2251e51f33
krzysm2 <m.krzysiek2003@gmail.com>
2023-05-26 13:41:22 +0200

    fixed chat and added dashboard

--- web/src/employee/dashboard/index.tsx
+++ web/src/employee/dashboard/index.tsx
@@ -1,7 +1,31 @@
-import {Box} from '@chakra-ui/react'
+import { selectProfile } from '@/auth/state'
+import LoadingView from '@/common/components/loading-view'
+import { useAppSelector } from '@/store'
+import {Box, Center, Progress} from '@chakra-ui/react'
 
 const Dashboard = () => {
-  return <Box p={4}>Dashboard</Box>
+  const user = useAppSelector(selectProfile)
+  
+  return !user ? <LoadingView/> : <Box w='100%' p={4}>
+    <Center w='100%' textAlign='center' fontSize='xl' fontWeight='bold'>
+      Dashboard
+    </Center>
+    <Center mb={5} w='100%' textAlign='center' fontSize=''>
+    {user.full_name} <br/>{user.company_role ? (user.company_role) : ('DevOps')}
+    </Center>
+    <Box mb={3}>
+      Tasks Completed: {user.points ? user.points : 77}
+    </Box>
+    <Progress height={6} value={(user.points ? user.points : 77)} />
+    <Box color='green'>
+      task 1 <br/>
+      task 2
+    </Box>
+    <Box color='red'>
+      task 3 <br/>
+      task 4
+    </Box>
+  </Box>
 }
 
 export default Dashboard
